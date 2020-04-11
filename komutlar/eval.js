d62d73f4ca1b1ad6e6427b61f27095c3c36ae44f
tarqu <43315422+tarqu@users.noreply.github.com>
2020-04-11 03:23:41 +0300

    Update eval.js

--- komutlar/eval.js
+++ komutlar/eval.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 exports.run = (client, message, args) => {
-	if (message.author.id === "698324571962736680") {
+	if (message.author.id === "698326997826404363") {
 		try {
 		  var code = args.join(" ");
 		  var evaled = eval(code);
